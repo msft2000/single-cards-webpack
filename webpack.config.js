4368002824c6316eedb629c33b3a9ad470c97b0f
Mateo Flores <dragonnegro1231@gmail.com>
2021-09-22 10:19:52 -0500

    Proyecto listo para despliegue a netlify

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWbpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports={
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
+    },
+    resolve:{
+        extensions: ['.js'],
+        alias:{
+            '@images': path.resolve(__dirname,'src/assets/images')
+        }
+    },
+    module:{
+        rules: [
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader'
+                ],
+            },
+            {
+                test: /\.png/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options:{
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[contenthash].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "../assets/fonts/",
+                        esModule: false,
+                    },
+                }
+            }
+        ]
+    },
+    plugins :[
+        new HtmlWbpackPlugin({
+            inject: true,
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        new Dotenv(),
+        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns:[
+                {
+                    from: path.resolve(__dirname,"src","assets/images"),
+                    to: "assets/images"
+                }
+            ]
+        })
+    ],
+    optimization:{
+        minimize: true
+    }
+}
